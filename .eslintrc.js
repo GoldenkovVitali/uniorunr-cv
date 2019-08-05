a4131eecbb682fdce1a2e6bf94699343260041fc
uniorunr <gagael23@live.com>
2019-08-05 23:38:58 +0300

    feat: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,4 +5,9 @@ module.exports = {
     jest: true,
   },
   parser: 'babel-eslint',
+  rules: {
+    'react/prefer-stateless-function': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
 }
