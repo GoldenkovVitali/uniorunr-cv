0699a980136b2dee2ce30f99673a0ba88b7e40eb
uniorunr <gagael23@live.com>
2019-08-07 11:58:56 +0300

    feat: fully implement summary (index) page; update styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import '../scss/generic.scss';
 import '../scss/index.scss';
+import { graphql, useStaticQuery } from 'gatsby';
 
-const IndexPage = () => <div>test</div>;
+const IndexPage = () => {
+  const {
+    contentfulSummary: { paragraphs },
+  } = useStaticQuery(
+    graphql`
+      query {
+        contentfulSummary {
+          paragraphs {
+            data
+            id
+          }
+        }
+      }
+    `,
+  );
+
+  return (
+    <Fragment>
+      {paragraphs.map(paragraph => (
+        <p key={paragraph.id}>{paragraph.data}</p>
+      ))}
+    </Fragment>
+  );
+};
 
 export default IndexPage;
